a791dc42743df30d61c9582ec494f95ca748673f
lleal24 <lorenaleal24@gmail.com>
2019-04-27 20:23:57 -0500

    Estructura y limpieza de codigo, creacion de archivo .jar para ejecutarlo fuera de netbeans

--- src/adivinadortcp_cliente/AdivinadorTCP_Cliente.java
+++ src/adivinadortcp_cliente/AdivinadorTCP_Cliente.java
@@ -12,12 +12,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  *
- * @author user
+ * @author Lorena Leal ID: 208313
+ *
  */
 public class AdivinadorTCP_Cliente {
 
@@ -25,34 +27,49 @@ public class AdivinadorTCP_Cliente {
         Socket socketCliente = null;
         BufferedReader entrada = null;
         PrintWriter salida = null;
+        //Descripcion del juego
+        System.out.println("----------------------------Adivinar el numero----------------------------------" + "\n"
+                + "El juego consiste en que el servidor"
+                + " generara un numero aleatorio entre 0 y 100." + "\n"
+                + "El objetivo es adivinar el numero teniendo en cuenta las pistas | tienes maximo 10 intentos" + "\n"
+                + "Buena Suerte.... ☻");
 
         try {
             //creacion del socket del lado cliente se define ip y socket de destino
-            socketCliente = new Socket("localhost", 4444);
+            socketCliente = new Socket("localhost", 4444);//192.168.56.1
             //Obtenemos canal de entrada
             entrada = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
             //Obtenemos canal de salida
             salida = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream())), true);
 
         } catch (IOException ex) {
+            //Mensaje que se mostrara en caso de no poder ejecutar correctamente el bloque try
             System.err.println("No puede establer conexión con el servidor");
+            //Salida del programa
             System.exit(-1);
         }
+        //@stdInd, variable tipo Buffer que almacenara el mensaje que el cliente desee enviar
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        //@linea, variable tipo string que almacenara el mensaje enviado por el servidor
         String linea;
 
         try {
             //Ciclo que se ejecuta 10 veces equivalente a los 10 intentos que tiene el cliente
             for (int i = 1; i <= 10; i++) {
-                //Captura de mensaje del cliente y envio al servidor
-                System.out.println("Ingresa numero:");
-                linea = stdIn.readLine();
-                salida.println(linea);
+                //Mensaje en patalla del cliente solicitando un numero
 
-                //Recepcion de mensaje del servidor e impresion
+                System.out.println("Ingresa numero:");
+                //Se guarda el valor que el cliente ingrese
+                linea = stdIn.readLine();
+                //envio del mensaje cliente/servidor/conversion a decimal
+                salida.println(bin(linea));
+
+                //Recepcion de mensaje del servidor
                 linea = entrada.readLine();
+                //impresion del mensaje
                 System.out.println("Ans Server: " + linea);
 
+                //USO DE EXPRESIONES REGULARES
                 //Creacion de patron palabara Clave al inicio del String "Adivinaste"
                 Pattern expresion = Pattern.compile("^Adivinaste.*");
                 //Evalua el String linea 
@@ -69,10 +86,17 @@ public class AdivinadorTCP_Cliente {
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
+
+        //Cierre del socket
         salida.close();
         entrada.close();
         stdIn.close();
         socketCliente.close();
     }
 
+    private static String bin(String a) {
+        String binario = Integer.toBinaryString(Integer.valueOf(a));
+        System.out.println("Numero binario enviado: " + binario);
+        return binario;
+    }
 }
